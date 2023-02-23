0c9a2ba19329e877202a5bc161a0355ab9615b0a
yeahg-dev <yeahg.dev@gmail.com>
2023-02-24 00:18:33 +0900

    Baekjoon: 피보나치 메모이제이션 잘못된 구현 삭제

--- ProblemSolving/AlgorithmImplementation/Recursion/Recursion_FIbonacci.swift
+++ ProblemSolving/AlgorithmImplementation/Recursion/Recursion_FIbonacci.swift
@@ -19,26 +19,8 @@ func fibonacci(_ n: Int) -> Int {
     }
 }
 
-// MARK: - Memoization
+// MARK: - For loop with Memoization
 // Memoization + Dynamic programming으로 시간복잡도 개선
-
-var result: [Int: Int] = [:]
-
-func fibonacciWithMemoization(_ n: Int) -> Int {
-    // 이미 계산한 적이 있다면, 결과값을 사용
-    if let result = result[n]  {
-        return result
-    }
-    
-    if (n == 1 || n == 2) {
-        return 1
-    } else {
-        result[n] = fibonacciWithMemoization(n-1) + fibonacciWithMemoization(n-2)
-    }
-    return fibonacciWithMemoization(n)
-}
-
-// MARK: - For loop
 
 func fibonacciLoop(_ n: Int) -> Int {
     var result = [0, 1, 1]
---
+++ ProblemSolving/BaekJoon/Recursion/10872_팩토리얼.swift
@@ -0,0 +1,8 @@
+//
+//  10872_팩토리얼.swift
+//  algorithm-practice
+//
+//  Created by Moon Yeji on 2023/02/23.
+//
+
+import Foundation
