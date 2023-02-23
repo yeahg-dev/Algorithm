c7dc0f767413d552a8aaac895279b68a177e5c4f
yeahg-dev <yeahg.dev@gmail.com>
2023-02-24 00:19:18 +0900

    Baekjoon: 10872_팩토리얼(재귀) 풀이 추가

--- ProblemSolving/BaekJoon/Recursion/10872_팩토리얼.swift
+++ ProblemSolving/BaekJoon/Recursion/10872_팩토리얼.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+// n에서 1씩 줄여가며 0이 될 때까지 곱셈을 반복한다.
+func solution10872() {
+    let n = Int(readLine()!)!
+    
+    print(factorial(n: n))
+    
+    
+    func factorial(n: Int) -> Int {
+        if n <= 1 {
+            return 1
+        }
+        return n * factorial(n: n-1)
+    }
+}
