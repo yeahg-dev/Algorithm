1eb94c21530d0a8492dfe12d29cd8616482addf2
yeahg-dev <yeahg.dev@gmail.com>
2023-04-13 17:40:28 +0900

    Beakjoon: 2178_미로탐색(BFS) 주석 추가

--- ProblemSolving/BaekJoon/BFS/2178_미로탐색.swift
+++ ProblemSolving/BaekJoon/BFS/2178_미로탐색.swift
@@ -26,17 +26,24 @@ func solution2178() {
     
     distance[0][0] = 1
     
+    // 1️⃣ Queue가 빌 때까지
     while !queue.isEmpty {
+        // 2️⃣ 가장 앞의 원소를 꺼낸 다음
         let node = queue.removeFirst()
         let previousDistance = distance[node.y][node.x]
         
+        // 3️⃣ 상하좌우 위치를 확인
         for i in 0..<4 {
             let y = node.y + dy[i]
             let x = node.x + dx[i]
             
+            // 4️⃣ 방문 가능하고, 아직 방문하지 않았다면
             if (0..<width).contains(x) && (0..<height).contains(y) && maze[y][x] == 1 {
+                // 1. 거리를 업데이트
                 distance[y][x] = previousDistance + 1
+                // 2. 큐에 다음 원소를 추가
                 queue.append((y, x))
+                // 3. 방문 처리
                 maze[y][x] = 0
                 
                 if x == width - 1 && y == height - 1 {
